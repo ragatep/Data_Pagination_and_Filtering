e7c534014fdae9b95b7efe68c0495669109b2960
ragatep <ragatep@gmail.com>
2021-12-05 16:28:06 -0800

    Project Starter Files

---
+++ js/script.js
@@ -0,0 +1,42 @@
+/*
+Treehouse Techdegree:
+FSJS Project 2 - Data Pagination and Filtering
+*/
+
+
+
+/*
+For assistance:
+   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
+   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
+*/
+
+
+
+/*
+Create the `showPage` function
+This function will create and insert/append the elements needed to display a "page" of nine students
+*/
+function showPage(list, page) {
+   // create two variables which will represent the index for the first and last student on the page
+ 
+   // select the element with a class of `student-list` and assign it to a variable
+ 
+   // set the innerHTML property of the variable you just created to an empty string
+ 
+   // loop over the length of the `list` parameter
+     // inside the loop create a conditional to display the proper students
+       // inside the conditional:
+         // create the elements needed to display the student information
+         // insert the above elements
+ }
+
+
+/*
+Create the `addPagination` function
+This function will create and insert/append the elements needed for the pagination buttons
+*/
+
+
+
+// Call functions
