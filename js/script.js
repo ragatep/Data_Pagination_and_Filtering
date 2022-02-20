16a1bf63d905897ffeac6ecdbe5637a0f927244d
ragatep <ragatep@gmail.com>
2022-02-19 20:39:05 -0800

    Refactored comments

--- js/script.js
+++ js/script.js
@@ -14,17 +14,16 @@ elements for a set of nine students per page.
 ********************************************/
 
 function showPage(list, page) {
-
   // Create the index start and end for the loop.
   const startIndex = (page * itemsPerPage) - itemsPerPage;
   const endIndex = (page * itemsPerPage);
-  
   // Select the UL element student-list class and clear its innerHTML property.
   const studentList = document.querySelector('.student-list');
   studentList.innerHTML = '';
-
-  // Build the student cards' HTML elements by iterating though 
-  // the length of the student's list.
+  /**
+   * Build the student cards' HTML elements by iterating though
+   *  the length of the student's list.
+   */
   let studentItem = '';
   for (let i = 0; i < list.length; i++){
     if(i >= startIndex && i < endIndex ){
@@ -42,7 +41,7 @@ function showPage(list, page) {
     }
   }
   studentList.insertAdjacentHTML('beforeend', studentItem);
- }
+}
 
 /********************************************
 The addPagination function
@@ -51,18 +50,19 @@ elements needed for the page buttons
 *********************************************/
 
 function addPagination(list) {
-
-  // Use some basic math that rounds a number up to the next 
-  // largest integer to create a variable to calculate the number 
-  // of pages needed.
+  /**
+   * Use some basic math that rounds a number up to the next
+   *  largest integer to create a variable to calculate the number
+   *  of pages needed.
+   */
   const numOfPages = Math.ceil(list.length / itemsPerPage)
-
   // Select the UL element link-list class and clear its innerHTML property.
   const linkList = document.querySelector('.link-list');
   linkList.innerHTML = '';
-
-  // Build the page buttons element by interating through 
-  // the numOfPages starting at 1.
+  /**
+   * Build the page buttons element by interating through
+   *  the numOfPages starting at 1.
+   */
   for (i = 1; i <= numOfPages; i++) {
     let button = `
     <li>
@@ -70,20 +70,23 @@ function addPagination(list) {
     </li>`;
     linkList.insertAdjacentHTML('beforeend', button);
   }
-
   // Variable to store NodeList of DOM buttons
   const buttons = document.querySelectorAll('button');
-
-  // The active class is added to the first pagination button
-  // when the application loads.
+  /**
+   * The active class is added to the first pagination button
+   *  when the application loads.
+   */
   buttons[0].classList.add("active");
-
-  // An event listener on linkList that sets a button to active 
-  // when clicked and displays the number of students for that page.
-  // No other elements should register on the click event.
+  /**
+   * An event listener on linkList that sets a button to active
+   *  when clicked and displays the number of students for that page.
+   *  No other elements should register on the click event.
+   */
   linkList.addEventListener('click', (e) => {
-    // TODO: refactor and maybe create a function named setAction(e). 
-    // Project Warm up - Where's the Action
+    /**
+     * TODO: refactor and maybe create a function named setAction(e).
+     * From Project Warm up - Where's the Action
+     */
     if (e.target.tagName === 'BUTTON') {
       for (let i = 0; i < buttons.length; i++){
         buttons[i].classList.remove('active');
@@ -91,13 +94,11 @@ function addPagination(list) {
       const buttonTarget = e.target;
       buttonTarget.classList.add('active');
       showPage(list, buttonTarget.textContent);
-
       //console.log(buttonTarget.textContent);
       //console.log(buttonTarget);
     }   
   });
 }
-
 // Call functions
 showPage(data, 1);
 addPagination(data);
